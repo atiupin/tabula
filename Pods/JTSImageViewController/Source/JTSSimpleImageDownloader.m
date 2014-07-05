5363c5a636d10d036fe55d873f052ee1248da613
Alex Tewpin <alex.tewpin@gmail.com>
2014-07-05 19:57:44 +0400

    Corrupted jpegs warning

--- Pods/JTSImageViewController/Source/JTSSimpleImageDownloader.m
+++ Pods/JTSImageViewController/Source/JTSSimpleImageDownloader.m
@@ -63,11 +63,24 @@
             image = [JTSAnimatedGIFUtility animatedImageWithAnimatedGIFData:data];
         }
         if (image == nil) {
-            image = [[UIImage alloc] initWithData:data];
+            
+            if ([referenceURL.pathExtension isEqualToString:@"jpg"] && ![self isJPEGValid:data]) {
+                image = [UIImage imageNamed:@"CorruptedJpeg"];
+            } else {
+                image = [[UIImage alloc] initWithData:data];
+            }
         }
     }
-    
     return image;
+}
+
++ (BOOL)isJPEGValid:(NSData *)jpeg {
+    if ([jpeg length] < 4) return NO;
+    const unsigned char * bytes = (const unsigned char *)[jpeg bytes];
+    if (bytes[0] != 0xFF || bytes[1] != 0xD8) return NO;
+    if (bytes[[jpeg length] - 2] != 0xFF ||
+        bytes[[jpeg length] - 1] != 0xD9) return NO;
+    return YES;
 }
 
 @end
